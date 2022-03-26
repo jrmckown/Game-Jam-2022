d416dc2a6acfd475af78ea165443ff9cc96cf4c5
jrmckown <jrmckown@ksu.edu>
2022-03-26 16:11:32 -0500

    added physics

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -11,15 +11,34 @@ public class PlayerMovement : MonoBehaviour
 
     public float runSpeed = 40f;
 
+    bool jump = false;
+    bool crouch = false;
+
     // Update is called once per frame
     void Update()
     {
 
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetButtonDown("Crouch"))
+        {
+            crouch = true;
+        }
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouch = false;
+        }
     }
 
     private void FixedUpdate()
     {
-        controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
+        jump = false;
+
     }
 }
